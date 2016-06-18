df27390806cca92bca25f1d9d1b681f3fec3b73e
Brian <bbensch@gmail.com>
2016-06-18 12:10:58 -0400

    heroku no dev errors, wrong domain?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ SnowSchoolers::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'skischoolco.herokuapp.com',
+    domain:               'skischoolers.herokuapp.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
