009d6c8d2202eacd9abdfd4529ee6d6c7ef24d5f
Brian <bbensch@gmail.com>
2016-06-18 11:26:41 -0400

    fix app.rb for local" "

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SnowSchoolers
     # config.i18n.default_locale = :de
   #LOAD local ENV variables
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      env_file = File.join(Rails.root, 'config', 'local_secrets.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
             end if File.exists?(env_file)
